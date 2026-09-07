cd7c5e7ca9bc25a8601eb275c1266fb7e78d759c
james-202608 <jameszhao@msn.com>
2026-09-07 10:52:03 +0800

    Correct five-region province grouping

--- china-map.js
+++ china-map.js
@@ -1,9 +1,9 @@
 (function(){
   const groups={
     northeast:{ko:'동북',cn:'东北',provinces:['辽宁省','吉林省','黑龙江省']},
-    north:{ko:'화북',cn:'华北',provinces:['北京市','天津市','河北省','山西省','内蒙古自治区','山东省']},
-    southeast:{ko:'동남',cn:'东南',provinces:['上海市','江苏省','浙江省','安徽省','福建省','江西省','台湾省','河南省','湖北省','湖南省','广东省','广西壮族自治区','海南省','香港特别行政区','澳门特别行政区']},
-    southwest:{ko:'서남',cn:'西南',provinces:['重庆市','四川省','贵州省','云南省','西藏自治区']},
+    north:{ko:'화북',cn:'华北',provinces:['北京市','天津市','河北省','山西省','内蒙古自治区','山东省','河南省']},
+    southeast:{ko:'동남',cn:'东南',provinces:['上海市','江苏省','浙江省','安徽省','福建省','江西省','广东省','广西壮族自治区','海南省','香港特别行政区','澳门特别行政区','台湾省']},
+    southwest:{ko:'서남',cn:'西南',provinces:['湖北省','湖南省','重庆市','四川省','贵州省','云南省','西藏自治区']},
     northwest:{ko:'서북',cn:'西北',provinces:['陕西省','甘肃省','青海省','宁夏回族自治区','新疆维吾尔自治区']}
   };
   const provinceGroup={}; Object.entries(groups).forEach(([key,g])=>g.provinces.forEach(p=>provinceGroup[p]=key));
